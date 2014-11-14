1f224037bba166436d7674d51abe2faad1bded28
Franco D. Ocaranza <franco@cambiamundos.com>
2014-11-14 16:52:28 -0300

    Ultimo commit - Práctico #1

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
 
 $(document).ready(function(){
-	
-	// MENU
-	/*$('#menu>li').hover(
-		function(){
-		$('.submenu',this).stop(true,true).fadeIn('fast');
-		},
-		function(){
-		$('.submenu',this).fadeOut('slow');
-		}
-	);*/
+
+	// BARRA LATERAL IZQUIERDA
 
 	$('#sidebar').hover(function(){
 		$(this).addClass('sidebar');
@@ -18,6 +10,59 @@ $(document).ready(function(){
 		$(this).removeClass('sidebar');
 		$('.information').hide();
 	});
+
+
+	// MENÚ DE USUARIO
+
+	$('#user-options, #user-options-menu').hover(function(){
+		$('#user-options-menu').show();
+	}, function(){
+		$('#user-options-menu').hide();
+	});
+
+
+	// BARRA LATERAL DERECHA
+
+	$('#user-options-menu li a').click(function(){
+		$('#chatbar').show();
+	});
+
+	$('#chatbar-out').click(function(){
+		$('#chatbar').hide();
+	});
+
+	$('#chatbar').mouseleave(function(){
+		$('#chatbar').fadeOut();
+	});
+
+
+	// TABLA DESPLEGABLE
+
+	$("#interviews tr:odd").addClass("odd");
+    $("#interviews tr:not(.odd)").hide();
+    $("#interviews tr:first-child").show();
+    
+    $("#interviews tr.odd").click(function(){
+        $(this).next("tr").toggle(200);
+        $(this).find(".arrow").toggleClass("up");
+    });
+
+
+    $('.comment').focus(function(){
+    	valor = $(this).val();
+    	if(valor=="Write the reason why you can't accept."){
+    		$(this).val('');
+    	}
+    });
+
+    $('.comment').blur(function(){
+    	valor = $(this).val();
+    	if(valor==""){
+    		$(this).val("Write the reason why you can't accept.");
+    	}
+    });
+
+
 
 	
 });
